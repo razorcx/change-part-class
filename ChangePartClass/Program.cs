5090000e3fd8cd3a6b6bfb3cd00e26b16e00725d
Cory LePage <cory@lepagegroup.ca>
2017-11-22 16:33:48 -0800

    Initial commit

--- ChangePartClass/Program.cs
+++ ChangePartClass/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
 
 namespace ChangePartClass
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			try
+			{
+				ChangePartClass.Run();
+			}
+			catch(Exception ex)
+			{
+				Trace.WriteLine(ex.InnerException + ex.Message + ex.StackTrace);
+			}
+		}
+
+		public static class ChangePartClass
+		{
+			private static readonly Dictionary<string, string> NameClassDictionary =
+				new Dictionary<string, string>()
+				{
+					{ "BEAM", "3" },
+					{ "COLUMN", "7" },
+					{ "JOIST", "11" },
+					{ "GIRT", "5" },
+					{ "BRACE", "4" },
+				};
+
+			public static void Run()
+			{
+				var pickedObjects = new Picker()
+					.PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS);
+				if (pickedObjects.GetSize() < 1) return;
+
+				while (pickedObjects.MoveNext())
+				{
+					var part = pickedObjects.Current as Part;
+					if(part == null)
+						continue;
+
+					var partName = part.Name.ToUpper();
+
+					if (NameClassDictionary.ContainsKey(partName))
+					{
+						part.Class = NameClassDictionary[partName];
+						part.Modify();
+					}
+				}
+
+				new Model().CommitChanges();
+			}
 		}
 	}
 }
